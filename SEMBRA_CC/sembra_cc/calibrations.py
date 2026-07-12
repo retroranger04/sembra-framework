1e02a13bcae4cfbd6c2ff887b8a8a1f3f2702f96
Arpit Mathur <retroranger24@gmail.com>
2026-07-12 13:07:06 +0530

    Remove v1 SEMBRA_CC folder (superseded by sembra_cc)

--- SEMBRA_CC/sembra_cc/calibrations.py
+++
@@ -1,8 +0,0 @@
-"""Centralized calibration constants used across the package.
-
-Defined here so any drift is traceable to one location.
-"""
-
-P_ZERO_THRESHOLD = 1e-6
-GREY_ZONE_HALF_WIDTH = 0.005
-LAMBDA_PHYS = 5.0
